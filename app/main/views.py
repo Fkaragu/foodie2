1ade6de2a6275e077cbe21425fb93fa28f89f384
Francis Karagu <fkaragu@gmail.com>
2019-02-19 22:23:59 +0300

    Added comment section

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..models import User
+from .forms import CommentForm
 
 # Views
 @main.route('/')
@@ -16,26 +17,5 @@ def options():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Welcome to the foodie app'
-    return render_template("search.html")
-
-@main.route('/comments/<int:id>',methods = ['GET','POST'])
-def pitch(id):
-
-    my_pitch = BLOG.query.get(id)
     comment_form = CommentForm()
-
-    if id is None:
-        abort(404)
-
-    if comment_form.validate_on_submit():
-        comment_data = comment_form.comment.data
-        new_comment = Comment(c_content = comment_data, c_blog_id = id, c_com_posted_on = datetime.now())
-        new_comment.save_comment()
-
-        return redirect(url_for('main.pitch',id=id))
-
-    all_comments = Comment.get_comments(id)
-
-    title = 'Comment Section'
-    return render_template('comment.html',pitch = my_pitch, comment_form = comment_form, comments = all_comments, title = title)
+    return render_template("search.html", comment_form = comment_form)
