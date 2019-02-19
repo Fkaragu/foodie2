6078a393280981087b5dc27913b4ffa6d9e4de47
Francis Karagu <fkaragu@gmail.com>
2019-02-19 15:30:03 +0300

    Modified the init file

--- app/__init__.py
+++ app/__init__.py
@@ -33,6 +33,4 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/auth')
 
-    configure_uploads(app)
-
     return app
