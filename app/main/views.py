5ad022649910f21d8602c644b7a76219fa5aad34
Francis Karagu <fkaragu@gmail.com>
2019-02-19 15:34:40 +0300

    Modified the template folder

--- app/main/views.py
+++ app/main/views.py
@@ -9,4 +9,4 @@ def index():
     View root page function that returns the index page and its data
     '''
     title = 'Welcome to the foodie app'
-    return render_template("index.html", title=title)
+    return render_template("index.html")
