672ef55a6267d7b696c9d6243913b9289203a2ca
joseck ogachi <jogachi4@gmail.com>
2019-02-19 18:41:27 +0300

    Deploy

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..models import User
+import requests
+import json
 
 # Views
 @main.route('/')
@@ -10,3 +12,13 @@ def index():
     '''
     title = 'Welcome to the foodie app'
     return render_template("index.html")
+
+@main.route('/search')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    random = requests.get('https://www.food2fork.com/api/search?key=ccdac9a346b2142bac88c41c214a941c').json
+
+    return render_template("food.html", random = random)
