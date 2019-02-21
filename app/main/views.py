2c3deec8a892567743b083f5bc09386343d36637
joseck ogachi <jogachi4@gmail.com>
2019-02-21 15:34:25 +0300

    added search functionality for the API

--- app/main/views.py
+++ app/main/views.py
@@ -3,9 +3,11 @@ from datetime import datetime
 from ..models import User,Comment,Intcom
 from .forms import CommentForm,IntFormL
 from flask_login import login_required, current_user
+from ..requests import food2fork
 from flask import render_template,request,redirect,url_for
 import requests
 import json
+
 
 # Views
 @main.route('/')
@@ -22,7 +24,6 @@ def options():
 
     intial_form = IntFormL()
 
-
     if intial_form.validate_on_submit():
 
         comm = intial_form.comm.data
@@ -31,11 +32,15 @@ def options():
 
         return redirect(url_for('main.options'))
 
+    search_food = request.args.get('food_query')
 
+    if search_food:
+        return redirect(url_for('main.test', search_food = search_food))
+    else:
 
-    all_int_comms = Intcom.get_all_comms()
-    random = requests.get('https://www.food2fork.com/api/search?key=ccdac9a346b2142bac88c41c214a941c').json()
-    return render_template("allfoods.html", intial_form = intial_form, allcom = all_int_comms,random = random)
+        all_int_comms = Intcom.get_all_comms()
+        random = requests.get('https://www.food2fork.com/api/search?key=7e71bd44dcf56c231b336f76201f7b47&q=chicken').json()
+        return render_template("allfoods.html", intial_form = intial_form, allcom = all_int_comms,random = random)
 
 @main.route('/reply/<int:id>',methods = ['GET','POST'])
 def pitch(id):
@@ -57,3 +62,11 @@ def pitch(id):
 
     title = 'Comment Section'
     return render_template('comm.html',pitch = my_pitch, comment_form = comment_form, comments = all_comments, title = title)
+
+@main.route('/search/<search_food>')
+def test(search_food):
+    '''
+    View function to display the search results
+    '''
+    random = requests.get('https://www.food2fork.com/api/search?key=7e71bd44dcf56c231b336f76201f7b47&q='+search_food).json()
+    return render_template('test.html',random = random)
---
+++ app/requests.py
@@ -0,0 +1,104 @@
+import urllib.request
+import urllib.request
+import json
+from functools import partial
+
+
+class food2fork():
+	'''
+	Usage:
+		import food2fork
+		f2f = food2fork("My Api Key")
+		recipes = f2f.search("chicken, rice")
+		moreRecipes = f2f.search("chicken, rice", 2)
+	'''
+
+	def __init__(self,apiKey, debug=False):
+		self.apiKey ='1873c967fa1cf5176b92e1f58c3683df'
+		self.debugMode = debug
+
+		self.SEARCH_ENDPOINT = 'https://www.food2fork.com/api/search?key='
+		self.VIEW_ENDPOINT   = 'http://food2fork.com/api/get'
+		self.MAX_PAGESIZE = 30
+
+	def search(self,query,page = 1,pageSize = 30):
+
+		try:
+
+			#url = self._urlHelper(self.SEARCH_ENDPOINT,self.apiKey,'&',q=query)
+
+			url = self._urlHelper('https://www.food2fork.com/api/search?key=1873c967fa1cf5176b92e1f58c3683df&q=chicken')
+			contents = self._getUrlContents(url)
+			data = json.loads(contents)
+			if(self.debugMode): print(contents)
+			return data
+
+		except Exception as inst:
+			if(self.debugMode): print(inst)
+
+			return None
+
+	def getTopRated(self,page = 1,pageSize = 30):
+		if(pageSize > self.MAX_PAGESIZE): pageSize = self.MAX_PAGESIZE
+
+		try:
+
+			url = self._urlHelper(self.SEARCH_ENDPOINT,page=page,count=pageSize)
+			contents = self._getUrlContents(url)
+			data = json.loads(contents)
+			if(self.debugMode): print(contents)
+			return data
+
+		except Exception as inst:
+
+			if(self.debugMode): print(inst)
+			return None
+
+	def getTrending(self,page = 1,pageSize = 30):
+		try:
+
+			url = self._urlHelper(self.SEARCH_ENDPOINT,page=page,count=pageSize,sort='t')
+			contents = self._getUrlContents(url)
+			data = json.loads(contents)
+			if(self.debugMode): print(contents)
+			return data
+
+		except Exception as inst:
+
+			if(self.debugMode): print(inst)
+			return None
+
+	def getRecipe(self,recipeId):
+		try:
+
+			url = self._urlHelper(self.VIEW_ENDPOINT,rId=recipeId)
+			contents = self._getUrlContents(url)
+			data = json.loads(contents)
+			return data
+
+		except Exception as inst:
+			if(self.debugMode): print(inst)
+			return None
+
+	def _urlHelper(self,endpoint,**kwargs):
+
+		data = { 'key' : self.apiKey }
+		for key, value in kwargs.iteritems():
+			data[key] = value
+
+		if(debugMode):
+			print ("Url:", endpoint + '?' + urllib.urlencode(data))
+
+		return endpoint + '?' + urllib.urlencode(data)
+
+	def _getUrlContents(self,url):
+		try:
+
+			response = urllib2.urlopen(url)
+			contents = response.read()
+			return contents
+
+		except Exception as inst:
+
+			if(self.debugMode): print(inst)
+			return None
